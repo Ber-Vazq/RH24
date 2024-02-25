a5ce42407b157d600c8f5ecbc33c6a3e583c5822
Ber-Vazq <bvaq20@hotmail.com>
2024-02-24 20:54:46 -0600

    honestly I kinda dissassociated

--- scripts/menu_toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var nav = document.querySelector('nav');
-    if (nav.classList.contains('active')) {
-        nav.classList.remove('active');
-    } else {
-        nav.classList.add('active');
-    }
-}
