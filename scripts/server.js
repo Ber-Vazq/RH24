671005dfcc075a7fa613c54806971b9b3687d738
Ber-Vazq <bvaq20@hotmail.com>
2024-02-25 07:01:43 -0600

    gods please

--- scripts/server.js
+++ scripts/server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
