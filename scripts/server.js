a87357cabed74caf656a89cc91ff0e26227c499e
Ber-Vazq <bvaq20@hotmail.com>
2024-02-25 06:38:07 -0600

    oh gods i hope this works

---
+++ scripts/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from their respective directories
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Serve index.html from the root directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
